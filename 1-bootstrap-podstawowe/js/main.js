20775e0ac57af11c0ca0945a071898877077fac3
Paweł Białek <pawelbialek23@op.pl>
2018-03-05 15:11:05 +0100

    Initial commit

---
+++ 1-bootstrap-podstawowe/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log('czy działa');
+
+//$(function () {
+//    $('#carouselExampleControls').carousel({
+//        interval: 10000
+//    });   
+//});    //podobnie to samo co niżej tyle, że trochę wolniej działa
+
+$('#carouselExampleControls').carousel({
+    interval: 7500
+});
